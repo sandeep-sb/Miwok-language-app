149baa7b8bed9dc33e22cff855ccf994b01a182f
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-04-29 00:01:33 +0530

    Phrases in Miwok language added

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -3,12 +3,34 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> words = new ArrayList<>();
+
+        //populating words using Word.java
+        words.add(new Word("minto wuksus", "Where are you going?"));
+        words.add(new Word("tinnә oyaase'nә", "What is your name?"));
+        words.add(new Word("oyaaset...", "My name is..."));
+        words.add(new Word("michәksәs", "How are you feeling?"));
+        words.add(new Word("kuchi achit", "I'm feeling good."));
+        words.add(new Word("әәnәs'aa?", "Are you coming?"));
+        words.add(new Word("hәә’ әәnәm", "Yes, I'm coming."));
+        words.add(new Word("әәnәm", "I'm coming."));
+        words.add(new Word("yoowutis", "Let's go."));
+        words.add(new Word("әnni'nem", "Come here."));
+
+        //Custom ArrayAdapter
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
