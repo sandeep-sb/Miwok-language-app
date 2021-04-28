177473c943d12e5fdaad824d7667ac38d7332201
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-04-28 23:55:40 +0530

    Add files via upload
    Custom ArrayAdapter added.

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -3,12 +3,34 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+
+        //ArraList for storing colors
+        ArrayList<Word> words = new ArrayList<>();
+
+        //Word.java file allows to store two words.
+        words.add(new Word("weṭeṭṭi", "red"));
+        words.add(new Word("chokokki", "red"));
+        words.add(new Word("ṭakaakki", "red"));
+        words.add(new Word("ṭopoppi", "red"));
+        words.add(new Word("kululli", "red"));
+        words.add(new Word("kelelli", "red"));
+        words.add(new Word("ṭopiisә", "red"));
+        words.add(new Word("chiwiiṭә", "red"));
+
+        //Custom ArrayAdapter
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
@@ -3,12 +3,36 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyMembersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family_members);
+        setContentView(R.layout.word_list);
+
+        //ArrayList to store family member words
+        ArrayList<Word> words = new ArrayList<>();
+
+        // Word.java allows to add 2 words.
+        words.add(new Word("әpә", "father"));
+        words.add(new Word("әṭa", "wife"));
+        words.add(new Word("angsi", "son"));
+        words.add(new Word("tune", "dadugter"));
+        words.add(new Word("taachi", "older brother"));
+        words.add(new Word("chalitti", "younger brother"));
+        words.add(new Word("teṭe", "older sister"));
+        words.add(new Word("kolliti", "younger sister"));
+        words.add(new Word("ama", "grandmother"));
+        words.add(new Word("paapa", "grandfather"));
+
+        //Custom ArrayAdapter
+
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
