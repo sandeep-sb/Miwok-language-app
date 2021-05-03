86567e57f9013bb3d2fda421ddf96688f3b78165
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-05-03 17:45:45 +0530

    added ImageResouceID

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,10 +8,20 @@ public class Word {
     //String variable for default translation of a word
     private String mDefaultTranslation;
 
+    //Integer variable for storing image resource id
+    private int mImageResourceID;
+
     //Construct a new Word with initial values for Miwok word and default word.
     public  Word(String miwokWord, String defaultWord){
         mMiwokTranslation = miwokWord;
         mDefaultTranslation = defaultWord;
+    }
+
+    //Construct a new Word with initial values for Miwok and default words and corresponding image.
+    public Word(String miwokWord, String defaultWord, int imageResourceID){
+        mMiwokTranslation = miwokWord;
+        mDefaultTranslation = defaultWord;
+        mImageResourceID = imageResourceID;
     }
 
     //Get the default translation of the word
@@ -21,7 +31,13 @@ public class Word {
     }
 
     //Get the miwok translation of the word
-    public  String getMiwoktWord(){
+    public  String getMiwokWord(){
         return mMiwokTranslation;
+    }
+
+    //Get the image resource id
+    public int getImageResourceID(){
+
+        return mImageResourceID;
     }
 }
