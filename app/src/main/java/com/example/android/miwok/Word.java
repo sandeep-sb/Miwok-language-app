2b3e12438de00dc868a476c0a35ba84a50d1e933
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-05-10 13:58:01 +0530

    added sound resource ID

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -13,17 +13,22 @@ public class Word {
     //Integer variable for storing image resource id
     private int mImageResourceID = NO_IMAGE_PROVIDED;
 
+    //Integer variable for storing sound resource id
+    private int mSoundResourceID;
+
     //Construct a new Word with initial values for Miwok word and default word.
-    public  Word(String miwokWord, String defaultWord){
+    public  Word(String miwokWord, String defaultWord, int soundResourceID){
         mMiwokTranslation = miwokWord;
         mDefaultTranslation = defaultWord;
+        mSoundResourceID = soundResourceID;
     }
 
     //Construct a new Word with initial values for Miwok and default words and corresponding image.
-    public Word(String miwokWord, String defaultWord, int imageResourceID){
+    public Word(String miwokWord, String defaultWord, int imageResourceID, int soundResourceID){
         mMiwokTranslation = miwokWord;
         mDefaultTranslation = defaultWord;
         mImageResourceID = imageResourceID;
+        mSoundResourceID = soundResourceID;
     }
 
     //Get the default translation of the word
@@ -45,5 +50,10 @@ public class Word {
     //returns true if view has image
     public boolean hasImage(){
         return mImageResourceID != NO_IMAGE_PROVIDED;
+    }
+
+    //Get the sound resource id
+    public int getSoundResourceID(){
+        return mSoundResourceID;
     }
 }
