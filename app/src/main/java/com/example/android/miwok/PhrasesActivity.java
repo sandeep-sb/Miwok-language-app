2d6e5ccfd0d14c4937f1c41e439dbd5647f66020
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-05-10 23:24:14 +0530

    Add files via upload

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,17 +1,37 @@
 package com.example.android.miwok;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
+
+    //This listener gets triggered when the mediaPlayer has completed playing the audio file.
+    //This is created here instead of after audio start in order to avoid repeated object creation.
+    private final MediaPlayer.OnCompletionListener mCompletionListener = mediaPlayer -> releaseMediaPlayer();
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +54,25 @@ public class PhrasesActivity extends AppCompatActivity {
 
         //Custom ArrayAdapter
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_phrases);
-        ListView listView = (ListView) findViewById(R.id.list);
+        ListView listView = findViewById(R.id.list);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener((adapterView, view, position, id) -> {
 
             //Get the Word object at the position user clicked on
             Word selectedWord = words.get(position);
 
+            //releasing MediaPlayer object if it currently exists before assigning it to a new audio file.
+            releaseMediaPlayer();
+
             //Create and setup the MediaPlayer for the audio resource associated with the current word
             mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, selectedWord.getSoundResourceID());
 
             //Start the audio file
             mMediaPlayer.start();
+
+            //Checking if MediaPlayer is still active by
+            //performing async callbacks to mMediaPlayer.
+            mMediaPlayer.setOnCompletionListener(mCompletionListener);
         });
     }
 }
